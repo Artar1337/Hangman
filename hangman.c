ca7657a17a9ff07705b8857002456fe791f3dc52
Artar1337 <armastra@gmail.com>
2019-04-19 21:02:07 +0700

    Upgraded prototype of main menu by adding extra textures in src/img

--- hangman.c
+++ hangman.c
@@ -3,12 +3,28 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Hangman");
-    sf::Sprite sprite1;
-    sf::Texture texture1;
-    if (!texture1.loadFromFile("src/img/mainmenu.png")) {
-        printf("Can't open the texture!\n");
-    }
+    sf::Sprite sprite1, buttonstart, buttonexit, buttondevelopers,
+            buttonsettings;
+    sf::Texture texture1, tex_but_start, tex_but_exit, tex_but_developers,
+            tex_but_settings;
+
+    texture1.loadFromFile("src/img/mainmenu.png");
+    tex_but_start.loadFromFile("src/img/buttonstart.png");
+    tex_but_settings.loadFromFile("src/img/buttonsettings.png");
+    tex_but_developers.loadFromFile("src/img/buttondevelopers.png");
+    tex_but_exit.loadFromFile("src/img/buttonexit.png");
+
     sprite1.setTexture(texture1);
+    buttonstart.setTexture(tex_but_start);
+    buttondevelopers.setTexture(tex_but_developers);
+    buttonsettings.setTexture(tex_but_settings);
+    buttonexit.setTexture(tex_but_exit);
+
+    buttonstart.setPosition(640, 20);
+    buttonsettings.setPosition(640, 190);
+    buttondevelopers.setPosition(640, 360);
+    buttonexit.setPosition(640, 530);
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -18,6 +34,10 @@ int main()
 
         window.clear();
         window.draw(sprite1);
+        window.draw(buttonstart);
+        window.draw(buttonsettings);
+        window.draw(buttondevelopers);
+        window.draw(buttonexit);
         window.display();
     }
 
