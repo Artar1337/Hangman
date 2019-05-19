e81376e81ae7a4b64185e095abc25f65ef15d36e
Danil <danil_shapovalov00@mail.ru>
2019-05-19 13:43:03 +0700

    The game's algorithm is partially implemented, the transfer of player names is done

--- src/draw.h
+++ src/draw.h
@@ -6,11 +6,18 @@ void SettingsMenu();
 void DevelopersMenu();
 void CategoryMenu();
 void FriendNameMenu();
-void FriendWordMenu();
+void FriendWordMenu(wchar_t name1[], wchar_t name2[]);
 int ComputerGame(
         wchar_t word[],
         wchar_t format_word[],
         int* rand_print,
         int t,
         char* wordnum);
+int FriendGame(
+        wchar_t word[],
+        wchar_t format_word[],
+        int t,
+        wchar_t name1[],
+        wchar_t name2[],
+        int* flag_name);
 #endif
